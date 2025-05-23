99dffe55d4a850a8544137d684becba9e8e0bf40
Monir <hossinmonir681@gmail.com>
2025-05-23 12:59:07 +0600

    about us section added

--- src/app/components/Home/Home.jsx
+++ src/app/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './Hero';
+import AboutUs from './AboutUs';
 
 const Home = () => {
     return (
         <div className='mt-12'>
             <Hero />
+            <AboutUs />
         </div>
     );
 };
